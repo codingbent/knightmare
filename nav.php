5607311ffb5b16bc19e51a5866759b9e7ccbd49c
abhed agarwal <abhed.agl@gmail.com>
2024-09-06 00:06:10 +0530

    edit

--- nav.php
+++ nav.php
@@ -57,12 +57,12 @@ $resultcategory=$con->query($sqlcategory);
 <div class="container">                           
     <nav class="navbar navbar-expand-lg navbar-light">
       <div class="container">
-                  <a class="navbar-brand fs-3 fw-bold" href="index.php"><img src="images/logo.jpg" style="width: 150px; height: auto; " alt=""></a>
+                  <a class="navbar-brand fs-3 fw-bold" href="index.php"><img src="images/logo.jpg" style="width:30px;transform:scale(4.0);" alt=""></a>
                   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                   </button>
                   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <form class="d-flex mx-auto "  onsubmit="return false;">
+                    <form class="d-flex mx-auto"  onsubmit="return false;">
                     <input class="form-control text" id="search_text" type="search" placeholder="Search" aria-label="Search" list="product">
                         <?php
             if ($resultcat->num_rows > 0) {
@@ -97,7 +97,7 @@ $resultcategory=$con->query($sqlcategory);
                           echo '<li><a class="dropdown-item" href="admin_dashboard.php">Admin Dashboard</a></li>';
                       }
                       if (@$_SESSION['is_admin'] == 0) {
-                        echo '<li><a class="dropdown-item" href="user_dashboard.php"> Dashboard</a></li>';
+                        echo '<li><a class="dropdown-item" href="user_dashboard.php">Admin Dashboard</a></li>';
                     }
                       
                       echo '<li><a class="dropdown-item" href="logout.php">Log Out</a></li>
